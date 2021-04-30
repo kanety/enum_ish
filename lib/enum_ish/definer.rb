19ea7cb42753785d2e9fd49d8ff22879fb4d18a8
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2021-04-30 17:16:54 +0900

    Add prefix option for short predicate

--- lib/enum_ish/definer.rb
+++ lib/enum_ish/definer.rb
@@ -32,8 +32,13 @@ module EnumIsh
 
     def define_predicate(enum)
       enum.mapping.each do |enum_key, enum_value|
+        if enum.setting[:predicate].is_a?(Hash) && enum.setting[:predicate][:prefix] == false
+          method_name = "#{enum_key}?"
+        else
+          method_name = "#{enum.name}_#{enum_key}?"
+        end
         @klass.class_eval do
-          define_method "#{enum.name}_#{enum_key}?".tr('.', '_') do
+          define_method method_name.tr('.', '_') do
             value = public_send(enum.name)
             if value.is_a?(Array)
               value == [enum_value]
