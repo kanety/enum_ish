b87b3d07d771fc39b81597964e3bbc0f287ddf70
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2021-08-21 15:02:45 +0900

    Fix test

--- spec/enum_ish/definer/exceptions_spec.rb
+++ spec/enum_ish/definer/exceptions_spec.rb
@@ -3,10 +3,16 @@ describe EnumIsh::Definer do
     let(:model) {
       test_model(Class.new) do
         include EnumIsh::Base
-        attr_accessor :val
+        attr_accessor :value
         enum_ish :value, ['val1', 'val2', 'val3']
       end
     }
+
+    it 'returns text' do
+      item = model.new
+      item.value = 'val1'
+      expect(item.value_text).to eq('val1')
+    end
 
     it 'returns options' do
       expect(model.value_options).to eq([["val1", "val1"], ["val2", "val2"], ["val3", "val3"]])
@@ -16,10 +22,16 @@ describe EnumIsh::Definer do
   context 'translation with same label' do
     let(:model) {
       test_model(UserModel) do
-        attr_accessor :val
+        attr_accessor :same_label_value
         enum_ish :same_label_value, ['val1', 'val2']
       end
     }
+
+    it 'returns text' do
+      item = model.new
+      item.same_label_value = 'val1'
+      expect(item.same_label_value_text).to eq('label')
+    end
 
     it 'returns options' do
       expect(model.same_label_value_options).to eq([['label', 'val1'],['label', 'val2']])
